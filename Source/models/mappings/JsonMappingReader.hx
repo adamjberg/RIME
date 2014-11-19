8636364587c8112fd9d1a8c132547cd078646ca9
dogwok <mikejensen21@gmail.com>
2014-11-18 22:03:43 -0800

    Restructured mapping interpretation to be more modular, as well as be able to interpret static properties of viper commands

--- Source/models/mappings/JsonMappingReader.hx
+++ Source/models/mappings/JsonMappingReader.hx
@@ -48,6 +48,7 @@ class JsonMappingReader {
                     case(MappingData.TYPE_SENSOR):
                         var sensorName:String = mappingDataObj.sensor;
                         var sensorData:SensorData = null;
+                        var staticParameters:Array<ViperCommand> = getArrayOfStaticParameters(mappingDataObj.staticParameters); 
                         if(mappingDataObj.rawData != null && mappingDataObj.rawData == "true")
                         {
                             sensorData = sensorDataController.getRawWithName(sensorName);
@@ -59,9 +60,9 @@ class JsonMappingReader {
 
                         if(sensorData != null)
                         {
-                            var targetFields:Array<String> = mappingDataObj.targetFields;
-                            var minOutputs:Array<Float> = mappingDataObj.minOutputs;
-                            var maxOutputs:Array<Float> = mappingDataObj.maxOutputs;
+                            var targetFields:Array<String> = mappingDataObj.dynamicParameters.targetFields;
+                            var minOutputs:Array<Float> = mappingDataObj.dynamicParameters.minOutputs;
+                            var maxOutputs:Array<Float> = mappingDataObj.dynamicParameters.maxOutputs;
                             mappingData = new SensorMappingData(
                                 sensorData,
                                 mappingDataObj.intervalInMs,
@@ -70,36 +71,60 @@ class JsonMappingReader {
                                 minOutputs,
                                 maxOutputs
                             );
-                        }
-                        else
+
+                             if(staticParameters != null){
+                                trace("static parameters don't equal null. I've got viper comands");
+                                for(staticParameter in staticParameters){
+                                    mappingData.addViperCommand(staticParameter);
+                                }
+                            } else {
+                                trace("static prameters equals null DUUDE!");
+                            }
+                        } else
                         {
                             trace("Could not map to sensor: " + sensorName);
                         }
 
+                       
+
+                        
+
+                    //Add piano specific data to the piano mapping 
                     case(MappingData.TYPE_PIANO):
-                        var viperCommands:Array<ViperCommand> = getArrayOfViperCommandsFromJson(mappingDataObj.commands);
+                    var staticParameters:Array<ViperCommand> = getArrayOfStaticParameters(mappingDataObj.staticParameters);
                         mappingData = new PianoMappingData
                         (
                             pianoButtons[mappingDataObj.buttonId],
                             mappingDataObj.pressType
                         );
-                        for(viperCommand in viperCommands)
-                        {
-                            mappingData.addViperCommand(viperCommand);
+                    if(staticParameters != null){
+                        trace("static parameters don't equal null. I've got viper comands");
+                        for(staticParameter in staticParameters){
+                            mappingData.addViperCommand(staticParameter);
                         }
+                    } else {
+                        trace("static prameters equals null DUUDE!");
+                    }    
 
+                    //Add gesture specific data to the gesture mapping 
                     case(MappingData.TYPE_GESTURE):
-                        var viperCommands:Array<ViperCommand> = getArrayOfViperCommandsFromJson(mappingDataObj.commands);
+                    var staticParameters:Array<ViperCommand> = getArrayOfStaticParameters(mappingDataObj.staticParameters);
                         mappingData = new GestureMappingData
                         (
                             gestureController,
                             mappingDataObj.gestureId
-                        );
-                        for(viperCommand in viperCommands)
-                        {
-                            mappingData.addViperCommand(viperCommand);
+                        );   
+                    if(staticParameters != null){
+                        trace("static parameters don't equal null. I've got viper comands");
+                        for(staticParameter in staticParameters){
+                            mappingData.addViperCommand(staticParameter);
                         }
+                    } else {
+                        trace("static prameters equals null DUUDE!");
+                    } 
                 }
+
+                //add all the data to the mapping object 
                 mapping.addMappingData(mappingData);
             }
 
@@ -111,33 +136,42 @@ class JsonMappingReader {
         return mapping;
     }
 
-    private function getArrayOfViperCommandsFromJson(jsonCommands:Array<Dynamic>):Array<ViperCommand>
+    private function getArrayOfStaticParameters(jsonStaticParameters:Array<Dynamic>):Array<ViperCommand>
     {
         var viperCommands:Array<ViperCommand> = new Array<ViperCommand>();
         var viperCommand:ViperCommand;
 
-        if(jsonCommands == null)
+        trace("getting Array of Static parameters"); 
+        if(jsonStaticParameters == null)
         {
+            trace("jsonstatic parmemters equals null dawg!");
             return viperCommands;
         }
 
-        for(jsonCommand in jsonCommands)
+        for(jsonStaticParameter in jsonStaticParameters)
         {
-            var targetFields:Array<Dynamic> = jsonCommand.targetFields;
-            var targetOutputs:Array<Dynamic> = jsonCommand.targetOutputs;
-            var method:String = jsonCommand.method;
+            var staticParameters:Array<Dynamic> = jsonStaticParameter.targetFields;
+            var staticParameterValues:Array<Dynamic> = jsonStaticParameter.targetOutputs;
+            var method:String = jsonStaticParameter.method;
+
+            trace("json static param method :" + method); 
 
             if(method == null)
             {
-                method = "update";
+                method = "update"; 
             }
 
             viperCommand = new ViperCommand();
-            viperCommand.method = method;
+            viperCommand.method = method; 
 
-            for(i in 0...targetFields.length)
-            {
-                viperCommand.addParam(targetFields[i], targetOutputs[i]);
+            if(staticParameters != null){
+                for(i in 0...staticParameters.length)
+                {
+                    viperCommand.addParam(staticParameters[i], staticParameterValues[i]);
+                    trace("Static Parameter Target Field: " + staticParameters[i]);
+                    trace(method); 
+                    trace("Static Paremeber Target Outputs: " + staticParameterValues[i]);
+                }
             }
 
             viperCommands.push(viperCommand);
