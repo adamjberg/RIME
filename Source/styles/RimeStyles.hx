b18258bd5a118a11b49425dcea8b75d0150f6136
Adam Berg <adam@xyzdigital.com>
2014-11-23 12:09:58 -0800

    Fix popup width

--- Source/styles/RimeStyles.hx
+++ Source/styles/RimeStyles.hx
@@ -1,7 +1,9 @@
 package styles;
 
 import haxe.ui.toolkit.core.Client;
+import haxe.ui.toolkit.core.PopupManager;
 import haxe.ui.toolkit.core.Screen;
+import haxe.ui.toolkit.core.Toolkit;
 import haxe.ui.toolkit.style.Style;
 import haxe.ui.toolkit.style.Styles;
 import openfl.filters.DropShadowFilter;
@@ -10,6 +12,8 @@ import openfl.Assets;
 class RimeStyles extends Styles {
     public function new() {
         super();
+
+        PopupManager.instance.defaultWidth = Math.round((Client.instance.windowWidth * 0.9) / Toolkit.scaleFactor);
 
         addStyle("Button.expandable", new Style
             (
